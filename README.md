95651dea3b732baca48040a4092c12c7d86ecd17
FavioA <134189396+FavioA@users.noreply.github.com>
2023-08-08 00:52:55 -0700

    Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # no-more-dawdling
 Workday Scheduler
+
+## This Challenge repository contains the following:
+
+> * A website that WHEN I open the planner, THEN the current day is displayed at the top of the calendar.
+>
+> * A website that WHEN I scroll down, THEN I am presented with timeblocks for standard business hours of 9am - 5pm.
+>
+> * A website that WHEN I view the timeblocks for that day, THEN I view the timeblocks for that day. 
+>
+> * A website that WHEN I click into a timeblock, THEN I can enter an event.
+>
+> * A website that WHEN I click the save button for that timeblock, THEN the text for that event is saved in local storage.
+>
+> * A website that WHEN I refresh the page, THEN the saved events persist.
+
+## Screenshot ()
+![Screenshot of portfolio website.](/images/Portfolio-Site-Screenshot.png)
+
+## Link to deployed website 
+This site was built using [GitHub Pages](https://favioa.github.io/Faviolous/).
--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 // in the html.
 $(document).ready(function () {
   // TODO: Add a listener for click events on the save button.
-  $(".saveBtn").each(function (i) {
-    $(this).on("click", function () {
-      var value = $(this).siblings(".description").val();
+  $('.saveBtn').each(function (i) {
+    $(this).on('click', function () {
+      var value = $(this).siblings('.description').val();
       var name = $(this).parent().attr("id");
       console.log(this);
 
@@ -14,11 +14,11 @@ $(document).ready(function () {
       localStorage.setItem(name, value);
 
       //Temporary notification to show that text was saved to local storage.
-      $('.notification').addClass('show');
+      $(".notification").addClass("show");
 
       //Timer removes notification after 8 seconds
       setTimeout(function () {
-        $('.notification').removeClass('show');
+        $(".notification").removeClass("show");
       }, 8000);
     });
   });
@@ -33,10 +33,18 @@ $(document).ready(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
 
-  // Current hour from day.js
+  // Loop over time blocks to apply past, present, and future classes to each time block according to current hour.
+  function updateTime() {
+
+    // Current hour from day.js
   var presentTime = dayjs().hour();
+  $('.time-block').(function () {
+    var hourSlot = parseInt($(this).attr('id')).split('-')[1]);
+  })
+  }
+
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
@@ -47,34 +55,27 @@ $(document).ready(function () {
   var storage10 = localStorage.getItem("hour-10");
   $("#hour-10 .description").val(storage10);
 
-
   var storage11 = localStorage.getItem("hour-11");
   $("#hour-10 .description").val(storage11);
-
 
   var storage12 = localStorage.getItem("hour-12");
   $("#hour-12 .description").val(storage12);
 
-
   var storage13 = localStorage.getItem("hour-13");
   $("#hour-13 .description").val(storage13);
-
 
   var storage14 = localStorage.getItem("hour-14");
   $("#hour-14 .description").val(storage14);
 
-
   var storage15 = localStorage.getItem("hour-15");
   $("#hour-15 .description").val(storage15);
-
 
   var storage16 = localStorage.getItem("hour-16");
   $("#hour-16 .description").val(storage16);
 
-
   var storage17 = localStorage.getItem("hour-17");
   $("#hour-17 .description").val(storage17);
- 
- // TODO: Add code to display the current date in the header of the page.
+
+  // TODO: Add code to display the current date in the header of the page.
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });
